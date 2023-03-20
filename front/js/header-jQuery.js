7f3708e96fa8efc38002b03220a5afe8c60ab708
Yang Hee Tae <87000854+gmlxo@users.noreply.github.com>
2023-03-20 09:57:03 +0900

    header
    아마 완성?

--- front/js/header-jQuery.js
+++ front/js/header-jQuery.js
@@ -1,7 +1,17 @@
 var header = $(".proFile");
 var menu = $(".profile-menu");
 
-$(".proFile").hover(
+$(header).hover(
+    function () {
+        // over
+        $(menu).stop().slideDown("fast");
+    }, function () {
+        // out
+        $(menu).stop().slideUp("fast");
+    }
+);
+
+$(menu).hover(
     function () {
         // over
         $(menu).stop().slideDown("fast");
