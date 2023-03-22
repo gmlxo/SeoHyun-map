15ab03780ed1ebf1f5f5deaf8aaba3073bd89d24
gmlxo <87000854+gmlxo@users.noreply.github.com>
2023-03-22 15:51:32 +0900

    service
    거의 완선
    + footer 거의 완성

--- front/js/header-jQuery.js
+++ front/js/header-jQuery.js
@@ -28,5 +28,17 @@ function hoverMenu() {
 }
 
 function locationPopUp() {
-    window.open("/Terms_of_Use/location_based.html", "location pop", "width = 1000px, left = 1000, location = no");
+    var value = document.getElementById("terms").getAttribute('value-number');
+    var htmlLink = "#";
+    if(value == 1) {
+        // 서비스 이용약관
+        htmlLink = "/Terms_of_Use/service.html";
+    } else if(value == 2) {
+        // 개인정보처리방침
+        htmlLink = "#";
+    } else if(valuec == 3) {
+        // 위치 기반 서비스 이용약관
+        htmlLink = "/Terms_of_Use/location_based.html";
+    }
+    window.open(htmlLink, "location pop", "location = no");
 }
