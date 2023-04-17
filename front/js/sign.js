f8610c95217fbbb96117c80105e4f5ffd7ef71c5
gmlxo <87000854+gmlxo@users.noreply.github.com>
2023-04-17 16:41:05 +0900

    집 가연ㄴ

--- front/js/sign.js
+++ front/js/sign.js
@@ -1,9 +1,15 @@
+/* sign up */
+$("input").click( function() {
+    
+});
+
 /* 약관 */ 
 var terms_checkbox_all = $("#terms_all");
 var terms_checkbox_all_check = $("#terms_all:checked");
 var terms_checkbox = $("div.terms-box>label>input[type=checkbox]");
 var terms_checkbox_check = $("div.terms-box>label>input[type=checkbox]:checked");
 
+// 전체 선택 checkbox 클릭 시
 terms_checkbox_all.click(function() {
     console.log("전체 선택 : " + terms_checkbox_all.is(":checked"))
     if(terms_checkbox_all.is(":checked")) {
@@ -13,6 +19,7 @@ terms_checkbox_all.click(function() {
     }
 });
 
+// checkbox 전부 클릭 시
 terms_checkbox.click(function() {
     console.log("선택된 개수 : " + terms_checkbox_check.length);
 
