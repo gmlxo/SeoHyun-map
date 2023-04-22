400496d0cf1fc14e8e88012bb3dc268a2959f047
gmlxo <87000854+gmlxo@users.noreply.github.com>
2023-04-22 16:29:24 +0900

    sign up js

--- front/js/sign.js
+++ front/js/sign.js
@@ -1,50 +1,41 @@
 /* sign up */
 $("input#sign_user_pw").on("click keyup", function () {
-    var list = ["input#sign_user_pw", "span#sign_pw_null", "span#sign_pw_no"];
-    // var input = "input#sign_user_pw";
-    // var span = "span#sign_pw_null";
-    // var spanNO = "span#sign_pw_no";
-    var pwList = ["!", "@", "#", "$", "%", "^", "&", "*", "_"];
+    // input or span list
+    var list = ["input#sign_user_pw", "span#sign_pw_null"];
+    // password 가능 특수문자
+    var pwList = ["!", "@", "#", "$", "%", "^", "&", "*", "_", "?"];
 
-    if($(list[0]).val() != "") {
-        for(var i = 0; i<=pwList.length; i++) {
-            if($(list[0]).val().indexOf(pwList[i])>0 || $(list[1]).val().length > 7) {
-                inputHideORShow("#00f", list[0], list[1], list[2]);
+    if ($(list[0]).val() != "") {
+        for (var i = 0; i <= pwList.length; i++) {
+            if ($(list[0]).val().indexOf(pwList[i]) > 0 && $(list[0]).val().length > 7 && $(list[0]).val().length < 31) {
+                inputHideORShow("#00f", list[0], list[1]);
                 break;
+            } else if($(list[0]).val().length < 8 || $(list[0]).val().length > 30) {
+                $(list[1]).text("8자 이상 30자 이하로 적어주세요");
             } else {
-                inputHideORShow("#f00", list[0], list[2], list[1]);
+                $(list[1]).text("보안에 취약합니다.");
+                inputHideORShow("#f00", list[0], list[1]);
             }
         }
     } else {
-        inputHideORShow("#f00", list[0], list[2], list[1]);
+        $(list[1]).text("내용이 입력되지 않았습니다.");
+        inputHideORShow("#f00", list[0], list[1]);
     }
 });
 
-function inputHideORShow(color, inputID, SPAN1, SPAN2) {
+function inputHideORShow(color, inputID, SPAN) {
     $(inputID).css({ "border-bottom-color": color });
-    $(SPAN1).hide();
-    if(color == "#f00") {
-        $(SPAN2).show();
+    if(color == "#00f") {
+        $(SPAN).hide();
     } else {
-        $(SPAN2).hide();
+        $(SPAN).show();
     }
 }
 
-/* version 1 */
-// function inputHideORShow(inputID, inputSPAN) {
-//     if ($(inputID).val() == "") {
-//         $(inputID).css({ "border-bottom-color": "#f00" });
-//         $(inputSPAN).show();
-//     } else {
-//         $(inputID).css({ "border-bottom-color": "#00f" });
-//         $(inputSPAN).hide();
-//     }
-// }
-
-/* version 2 */
 let input = document.querySelectorAll("input.sign-input");
 let span = document.querySelectorAll("span.notNull-sign");
-input.forEach((e, index)=>{
+
+input.forEach((e, index) => {
     e.addEventListener("click", (e) => {
         inputALLHideORShow(input[index], span[index]);
     });
@@ -56,37 +47,34 @@ input.forEach((e, index)=>{
 function inputALLHideORShow(inputID, inputSPAN) {
     var inputPW = document.getElementById("sign_user_pw");
 
-    if(inputID != inputPW) {
-        if(inputID.value == "") {
-            $(inputID).css({ "border-bottom-color":"#f00" });
+    if (inputID != inputPW) {
+        if (inputID.value == "") {
+            $(inputID).css({ "border-bottom-color": "#f00" });
             $(inputSPAN).show();
+        } else {
+            $(inputID).css({ "border-bottom-color": "#00f" });
+            $(inputSPAN).hide();
         }
     }
 }
 
 /* 약관 */
-var terms_checkbox_all = $("#terms_all");
-var terms_checkbox_all_check = $("#terms_all:checked");
-var terms_checkbox = $("div.terms-box>label>input[type=checkbox]");
-var terms_checkbox_check = $("div.terms-box>label>input[type=checkbox]:checked");
+var terms_checkbox = ["#terms_all", "#terms_all:checked", "div.terms-box>label>input.terms_checkbox", "div.terms-box>label>input.terms_checkbox:checked"];
 
 // 전체 선택 checkbox 클릭 시
-terms_checkbox_all.click(function () {
-    console.log("전체 선택 : " + terms_checkbox_all.is(":checked"))
-    if (terms_checkbox_all.is(":checked")) {
-        terms_checkbox.prop('checked', true);
+$(terms_checkbox[0]).click(function () {
+    if ($(terms_checkbox[0]).is(":checked")) {
+        $(terms_checkbox[2]).prop('checked', true);
     } else {
-        terms_checkbox.prop('checked', false);
+        $(terms_checkbox[2]).prop('checked', false);
     }
 });
 
 // checkbox 전부 클릭 시
-terms_checkbox.click(function () {
-    console.log("선택된 개수 : " + terms_checkbox_check.length);
-
-    if (terms_checkbox_check.length >= 4) {
-        terms_checkbox_all_check.prop('checked', true);
+$(terms_checkbox[2]).click(function () {
+    if ($(terms_checkbox[3]).length > 3) {
+        $(terms_checkbox[0]).prop('checked', true);
     } else {
-        terms_checkbox_all_check.prop('checked', false);
+        $(terms_checkbox[0]).prop('checked', false);
     }
 });
